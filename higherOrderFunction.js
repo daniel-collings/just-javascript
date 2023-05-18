49333b5203c2d58d24e52d038fbc3705c702e452
Daniel Collings <collings8915@gmail.com>
2023-05-18 21:27:51 +0100

    HigherOrderFn takes array param and returns orginal result and new array

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,10 +1,8 @@
-const array = [ 1, 2, 3 ]
-
 function copyArrayAndManipulate(array, operand, instructions){
-    return array.map((num) => (instructions(num, operand)))
+    return [array, array.map((num) => (instructions(num, operand)))]
 }
 
-const result = copyArrayAndManipulate(array, 3, (input, operand) => input * operand)
+const [og, result] = copyArrayAndManipulate([ 1, 2, 3 ], 3, (input, operand) => input * operand)
 
-console.log(`Original Array: ${array}`)
+console.log(`Original Array: ${og}`)
 console.log(`Manipulated Array: ${result}`)
