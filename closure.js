1b177ccd88f2f61ef96580b81ab71d17b70421e4
Daniel Collings <collings8915@gmail.com>
2023-05-18 22:02:00 +0100

    Add calling Fn out of Fn where it was defined. Plus comments.

--- closure.js
+++ closure.js
@@ -6,3 +6,20 @@ const generatedFn = createFunction()
 const result = generatedFn(2)
 
 console.log(`Result: ${result}`)
+
+ // create execution context
+function outer(){
+    // local memory of the above context contains a reference to counter and incrementCounter function
+    let counter = 0
+
+    // creates its own execution context with local memory of counter.
+    // when it doesn't find counter in it's local memory, it'll then reach out to outer's context and increment that counter.
+    function incrementCounter(){ return counter++ } 
+
+    // returns function plus all of the local memory from the outer 
+    return incrementCounter 
+}
+
+const myNewFunction = outer()
+myNewFunction() // 1
+myNewFunction() // 2
