4628c9c33e45b4783dff391cbe33848ff4d27a82
Daniel Collings <collings8915@gmail.com>
2023-05-20 13:03:39 +0100

    Add notes for further understand

--- closure.js
+++ closure.js
@@ -14,12 +14,25 @@ function outer(){
 
     // creates its own execution context with local memory of counter.
     // when it doesn't find counter in it's local memory, it'll then reach out to outer's context and increment that counter.
-    function incrementCounter(){ return counter++ } 
+    function incrementCounter(){ 
+        console.log(counter)
+        return counter++ 
+    } 
 
     // returns function plus all of the local memory from the outer 
     return incrementCounter 
 }
 
 const myNewFunction = outer()
+myNewFunction() // 0
 myNewFunction() // 1
-myNewFunction() // 2
+
+const anotherNewFunction = outer() // 0 and 1 return due to seperate exec contexts being created
+anotherNewFunction() // 0
+anotherNewFunction() // 1
+
+// within the exec context the local memory is known as Variable Evironment (VE)
+// the closure/scoping/backpacking technical terming: closed over VE or Persistent Lexical Scope Reference Data.
+
+
+//closure ideal for helper functions where you only want to run the function once. An example of this is nth prime number. By memorising this you save on compute time.
